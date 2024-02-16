36a56c1e7cf7e1b0c966e9a7757c90558fa44199
Purav Dhanki <puravdhanki49@gmail.com>
2024-02-16 22:12:11 +0530

    Purav

---
+++ files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Photographer" , "Digital Poster Designer" , "Freelancer" , "Website Tester" , "Web Development" , "Video Editor" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1500,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
